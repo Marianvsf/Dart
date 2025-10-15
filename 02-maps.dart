cc5c4b5c9abc4a1ee8000b95653834f95b23566d
Marianvsf <marianvsf@gmail.com>
2025-10-15 11:33:46 -0400

    maps

---
+++ 02-maps.dart
@@ -0,0 +1,14 @@
+void main() {
+  final Map<String, dynamic> pokemon = {
+    'name': 'Ditto',
+    'hp': 100,
+    'isAlavie': true,
+    'habilities': <String>['Impostor'],
+    'sprites': {1: 'ditto/front.png', 2: 'ditto/back.png'}
+  };
+  print(pokemon);
+  print('Name: ${pokemon['name']}');
+  print('Name: ${pokemon['sprites']}');
+  print('Back: ${pokemon['sprites'][2]}');
+  print('Front: ${pokemon['sprites'][1]}');
+}
